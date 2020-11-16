606b15b9239fe0f2a9a07cba09bae7ea121ce519
nbutcher1 <22nbutcher@athenian.org>
2020-11-16 01:09:39 +0000

    finished

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,34 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
-
+    public String breed;
+    public String name;
+    public int age;
     // Constructors
-    
+    public Dog(String name, int age, String breed) {
+        super(name,age);
+        this.breed = breed;
+    }
+    public Dog() {
+        super();
+    }
 
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Woof!");
+    }
 
     // toString method
-
-
+    public String toString() {
+        String str = "Name: " + this.name + ", Age: " + this.age + ", Breed: " + this.breed;
+        return str;
+    }
     // Getter
-
+    public String getBreed() {
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String newb) {
+        this.breed = newb;
+    }
 }
